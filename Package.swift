e076a51158c24017ce253fb2508e9e3ef1e0d87e
littleappdevs <littledevelopers2021@gmail.com>
2022-02-16 16:42:39 +0300

    Package Readability Checks

--- Package.swift
+++ Package.swift
@@ -33,9 +33,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "LittleSDK",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources/LittleSDK"),
         .testTarget(
             name: "LittleSDKTests",
-            dependencies: ["LittleSDK"]),
+            dependencies: ["LittleSDK"],
+            path: "Tests/LittleSDKTests"),
     ]
 )
